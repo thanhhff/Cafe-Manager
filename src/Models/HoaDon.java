5ccb2d5dab2cc290e3a661ace461a46936ff0a5c
Thanhhff <thanhnguyen200399@gmail.com>
2020-04-30 13:21:46 +0700

    Cập nhật Method HoaDon và các phương thức Get / Set

--- src/Models/HoaDon.java
+++ src/Models/HoaDon.java
@@ -22,4 +22,69 @@ public class HoaDon {
     private int MaHoaDon, GiamGia, TongTien, TrangThai, MaBan;
     private Date GioDen;
 
+    public HoaDon() {
+        this.MaHoaDon = 0;
+        this.GiamGia = 0;
+        this.TongTien = 0;
+        this.TrangThai = 0;
+        this.MaBan = 0;
+        this.GioDen = null;
+    }
+
+    public HoaDon(int MaHoaDon, int GiamGia, int TongTien, int TrangThai, int MaBan, Date GioDen) {
+        this.MaHoaDon = MaHoaDon;
+        this.GiamGia = GiamGia;
+        this.TongTien = TongTien;
+        this.TrangThai = TrangThai;
+        this.MaBan = MaBan;
+        this.GioDen = GioDen;
+    }
+
+    public int getMaHoaDon() {
+        return MaHoaDon;
+    }
+
+    public void setMaHoaDon(int maHoaDon) {
+        MaHoaDon = maHoaDon;
+    }
+
+    public int getGiamGia() {
+        return GiamGia;
+    }
+
+    public void setGiamGia(int giamGia) {
+        GiamGia = giamGia;
+    }
+
+    public int getTongTien() {
+        return TongTien;
+    }
+
+    public void setTongTien(int tongTien) {
+        TongTien = tongTien;
+    }
+
+    public int getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        TrangThai = trangThai;
+    }
+
+    public int getMaBan() {
+        return MaBan;
+    }
+
+    public void setMaBan(int maBan) {
+        MaBan = maBan;
+    }
+
+    public Date getGioDen() {
+        return GioDen;
+    }
+
+    public void setGioDen(Date gioDen) {
+        GioDen = gioDen;
+    }
 }
