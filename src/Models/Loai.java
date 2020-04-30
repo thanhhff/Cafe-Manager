f22d62da5595ad7280755c09d7eafab6a240dcab
Thanhhff <thanhnguyen200399@gmail.com>
2020-04-30 12:35:23 +0700

    Cập nhật TODO

--- src/Models/Loai.java
+++ src/Models/Loai.java
@@ -6,9 +6,16 @@
 package Models;
 
 /**
+ * Model Loai: chứa cấu trúc về loại (nhóm các mặt hàng) về mã loại, tên loại, màu sắc
  *
  * @author thanhhff
  */
+
+//TODO: tạo các method Loai() có chữ ký và không có chữ ký;
+//TODO: hoàn thành các phương thức set / get;
+
 public class Loai {
-    
+
+    private String MaLoai, TenLoai, MauSac;
+
 }
