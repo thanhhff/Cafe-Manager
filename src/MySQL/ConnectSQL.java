15c67db826f3b156d669211a7d13aa40fef1a234
Thanhhff <thanhnguyen200399@gmail.com>
2020-05-26 08:28:27 +0700

    Update ConnectSQL.java

--- src/MySQL/ConnectSQL.java
+++ src/MySQL/ConnectSQL.java
@@ -28,7 +28,7 @@ public class ConnectSQL {
 
     public ConnectSQL() {
         try {
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlcafe?useUnicode=true&characterEncoding=utf8", "root", "");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/cafe-manager?useUnicode=true&characterEncoding=utf8", "root", "");
             System.out.println("Kết nối SQL thành công !");
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Lỗi ConnectSQL: kết nối tới SQL thất bại !");
