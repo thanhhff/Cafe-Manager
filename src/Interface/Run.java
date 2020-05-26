a0463c19bacdf195c93c9410cbfe622c7499640a
Thanhhff <thanhnguyen200399@gmail.com>
2020-05-26 08:16:46 +0700

    Update Run.java

--- src/Interface/Run.java
+++ src/Interface/Run.java
@@ -6,18 +6,21 @@ public class Run {
     public static frmMain QlCafe;
     public static frmLogIn frmlg;
     public static TaiKhoan tk;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         lg();
     }
-    public static void lg(){
+
+    public static void lg() {
         frmlg = new frmLogIn();
-        frmlg.setVisible(true);       
-    }  
-    public static void QLCF(){
+        frmlg.setVisible(true);
+    }
+
+    public static void QLCF() {
         QlCafe = new frmMain();
-        QlCafe.setVisible(true);       
+        QlCafe.setVisible(true);
     }
 }
